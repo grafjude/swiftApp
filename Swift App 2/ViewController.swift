450e884fac4d0ce7acc37af7c6736c8bd01d99e7
JG <grafjude@badinhs.org>
2016-09-14 08:20:30 -0400

    no message

--- Swift App 2/ViewController.swift
+++ Swift App 2/ViewController.swift
@@ -2,19 +2,36 @@
 //  ViewController.swift
 //  Swift App 2
 //
-//  Created by BHSRam8 on 9/6/16.
-//  Copyright © 2016 BHSRam8. All rights reserved.
+//  Created by Jude Graf on 9/6/16.
+//  Copyright © 2016 Jude Graf. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var coolLabel: UILabel!
+    
+    var tapCount=0
+    
+    @IBAction func button1Tapped(_ sender: AnyObject) {
+        coolLabel.text="Now Cooler!"
+        print("buttonTapped")
+        
+        tapCount = tapCount + 1
+        if tapCount==10 { coolLabel.text="You have tapped 10 times"
+        }
+        print(tapCount)
+}
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.view.backgroundColor = UIColor.darkGray
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
